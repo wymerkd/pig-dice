3cf34eb21c61d209d976ff7532b173fbd684d9df
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-02 16:01:38 -0700

    add comments to myself re: how the functions relate to each other

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 // business logic
+
+//this is the array where everything will go on the back end
 var sentenceLatin =[]
 
-
+//this is where highest level work gets done. The function looper is called by the user interface. Inside it, we have the ability to take individual words (from wordSplit, aka running .split on sentence), and push them into the final array.
 function looper(sentence) {
  var wordSplit = analyzer(sentence);
  wordSplit.forEach(function(word) {
@@ -11,13 +13,14 @@ function looper(sentence) {
  return sentenceLatin;
 }
 
+//this is where the user's sentence ends up getting the method .split so that within looper() we can treat each word separately.
 function analyzer(sentence) {
   var wordSplit = sentence.split(" ");
   return wordSplit
 }
 
 
-
+//this is where we figure out what will happen to each word. it's not true pig Latin, but I could go and add that functionality within this structure
 var pigLatinized = function latinizer(word) {
  if (word.length ===1) {
    return latinizerOne(word)
@@ -33,7 +36,7 @@ else {
 
 
 
-
+//these are the individual functions that get called within latinizer. They were written assuming that latinizer will sort out what goes where.
 function latinizerOne(word) {
   var newWord = word + "way"
   return newWord
