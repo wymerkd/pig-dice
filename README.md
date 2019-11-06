1642a6bc3a0b47083d10e9fc9001e9dda78030d0
Student <example@example.com>
2019-11-06 13:44:09 -0800

    add second array to capture total score for each player

--- README.md
+++ README.md
@@ -24,11 +24,13 @@ Write a program where two users can play Pig dice against each other. Don't forg
 HTML, CSS, Bootstrap 3.3.7, JavaScript, and jQuery v3.4.1 (uncompressed development version).
 Project was written using Google Chrome; no other browsers were tested for compatibility.
 
+The random number generator for our die was adapted from the JavaScript on this page: https://codepen.io/Pyremell/pen/eZGGXX/.
+
 ### Project Setup instructions
 There shouldn't be any special setup required. If something doesn't display correctly or goes wrong somehow, please contact us at <wymerkd@gmail.com> and <lara.m.bjork@gmail.com> and we will do our best to troubleshoot for you.
 
 ### Known Issues
-* 
+*
 
 
 ### License
--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@
 
 var scorePlayer1 = [];
 var scorePlayer2 = [];
+var totalPlayer1 = [];
+var totalPlayer2= [];
 
 
 var dicePlayer1 = {
@@ -17,7 +19,14 @@ var dicePlayer1 = {
     scorePlayer1.push(randomNumber)}
     console.log(scorePlayer1)
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    console.log(scorePlayer1.reduce(reducer));
+    var newReducer1 = scorePlayer1.reduce(reducer);
+    totalPlayer1.push(newReducer1);
+    console.log(newReducer1);
+    console.log(totalPlayer1);
+    // var newScorePlayer1 = n;
+    // return newScorePlayer1;
+    // totalPlayer1.push(newScorePlayer1);
+    // console.log(totalPlayer1)
   }
 };
 var dicePlayer2 = {
@@ -30,7 +39,10 @@ var dicePlayer2 = {
     scorePlayer2.push(randomNumber)}
     console.log(scorePlayer2)
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    console.log(scorePlayer2.reduce(reducer));
+    var newReducer2 = scorePlayer2.reduce(reducer);
+    totalPlayer2.push(newReducer2);
+    console.log(newReducer2);
+    console.log(totalPlayer2);
   }
 };
 
@@ -41,12 +53,12 @@ $(document).ready(function() {
   // var totalScore = new TotalScore();
   var rollButton1 = document.getElementById('roll-button-1');
   rollButton1.onclick = function(){
-    var result = dicePlayer1.roll();
-    $("#current-roll").html(result);
+    var result1 = dicePlayer1.roll();
+    $("#current-roll").html(result1);
   }
   var rollButton2 = document.getElementById('roll-button-2');
   rollButton2.onclick = function(){
-    var result = dicePlayer2.roll();
-    $("#current-roll").html(result);
+    var result2 = dicePlayer2.roll();
+    $("#current-roll").html(result2);
   }
 });
